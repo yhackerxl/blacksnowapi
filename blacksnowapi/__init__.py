6dab24818cd008533ec5e43024e677413df28ed5
David Mpinzile <davidnewton016@gmail.com>
2026-02-12 17:18:36 +0300

    Refactor imports to use SmartHouseAPI class

--- blacksnowapi/__init__.py
+++ blacksnowapi/__init__.py
@@ -1,18 +1,1 @@
-from .core import (
-    setup,
-    on,
-    off,
-    toggle,
-    fan_on,
-    fan_off,
-    alarm_on,
-    alarm_off,
-    open_door,
-    close_door,
-    toggle_door,
-    set_blinds,
-    blinds_open,
-    blinds_close,
-    set_thermostat,
-    wait
-)
+from .core import SmartHouseAPI
